0333f7330502b6628892a7873676a4668a1070d0
02NIN20 <lycoronelp@udistrital.edu.co>
2026-07-02 23:01:59 -0500

    add /health endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ from app.api.routes import router
 
 app = FastAPI(title="Synod", docs_url="/docs")
 app.include_router(router)
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
